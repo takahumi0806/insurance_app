27eb1710e9f2f115a86d10ab221a0df77a7bf14a
takahumi0806 <yabuta0439@hotmail.com>
2021-01-02 10:54:34 +0900

    Update Step3.vue

--- src/components/Step3.vue
+++ src/components/Step3.vue
@@ -6,7 +6,7 @@
     <font-awesome-icon icon='file-alt'></font-awesome-icon>
     ご相談内容をご記入ください<br>
     -ご相談内容-<br>
-    <textarea rows='10' cols='60' :value="message" @input="doUpdate"></textarea><br>
+    <textarea rows='10' cols='60' :value='message' @input='doUpdate'></textarea><br>
     <button><router-link to='/Step2'>前へ戻る</router-link></button>
     <button><router-link to='/Step4'>確認画面</router-link></button>
   </div>
